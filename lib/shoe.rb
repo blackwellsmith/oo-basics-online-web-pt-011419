18c1a123564170e5781b2812e98d27ecd9f1c49b
blackwell <blackwellsmithiv@gmail.com>
2019-01-29 14:58:29 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -27,6 +27,12 @@ def initialize(brand)
   def material
     @material
   end
+  def condition=(condition)
+    @condition = condition
+  end
+  def condition
+    @condition
+  end
    def turn_page
     puts "Flipping the page...wow, you read fast!"
   end
